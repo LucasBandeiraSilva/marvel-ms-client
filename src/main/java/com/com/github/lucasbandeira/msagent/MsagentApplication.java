427620be3ff6109628212ca4926aaec09dde141b
Lucas Bandeira <lucasbandeirai40@gmail.com>
2025-04-16 14:02:12 -0300

    chore: add application.yml with H2 database and Eureka configuration

---
+++ src/main/java/com/com/github/lucasbandeira/msagent/MsagentApplication.java
@@ -0,0 +1,13 @@
+package com.com.github.lucasbandeira.msagent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsagentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsagentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/com/github/lucasbandeira/msagent/MsagentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.com.github.lucasbandeira.msagent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsagentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
